9cef605a5d26e88bb73b9768f1c5ca4d815fc519
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2021-11-24 21:40:02 +0700

    Create DUYNO.cpp

---
+++ DUYNO.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    while(cin>>s){
+    if(s[0] == s[s.size()-1]) cout<<"YES"<<endl;   
+    else cout<<"NO"<<endl; 
+    }
+}
